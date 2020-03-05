321303be7948a200e95af7e1a9116223717893c2
sam-mfb <sam@mfbtech.com>
2020-03-05 16:15:52 -0500

    moved parsing logic into separate files so abstracted from React display issues

--- src/Planet.js
+++ src/Planet.js
@@ -1,22 +1,27 @@
+/* File: Planet.js
+ *
+ * React script to load a Continuum "Galaxy" file, parse it using
+ * the planet-parser routines and display a map of any planet from
+ * that file
+ *
+ * Sam Davidoff - 2020
+ */
+
 import React from "react"
-import GW from "./GW"
+import { parseGalaxyFile, getPlanet } from "./planet-parser"
 
 const Planet = () => {
   const canvasEl = React.useRef(null)
-  const galaxyRef = React.useRef(null)
-  const indexRef = React.useRef(null)
-
   const [currLevel, setCurrLevel] = React.useState(0)
   const [showError, setShowError] = React.useState(false)
   const [errorText, setErrorText] = React.useState("")
-  const [numPlanets, setNumPlanets] = React.useState(0)
-  const [demoPlanet, setDemoPlanet] = React.useState(0)
   const [showControls, setShowControls] = React.useState(false)
   const [canvasHeight, setCanvasHeight] = React.useState(0)
   const [canvasWidth, setCanvasWidth] = React.useState(0)
+  const galaxyRef = React.useRef(null)
 
   React.useEffect(() => {
-    if (galaxyRef.current !== null && indexRef.current !== null) {
+    if (galaxyRef.current !== null) {
       selectAndDrawPlanet()
     } else {
       setErrorText("You need to load a galaxy file")
@@ -24,12 +29,10 @@ const Planet = () => {
     }
   }, [currLevel, canvasWidth, canvasHeight])
 
-  const ylength = [0, 2, 2, 2, 1, 0] //basically a rough table that looks up 2xsin
-  const xlength = [0, 0, 1, 2, 2, 2] //basically a rough table that looks up 2xcos
-
   const handleNext = e => {
     e.preventDefault()
-    if (currLevel < numPlanets) setCurrLevel(prevLevel => prevLevel + 1)
+    if (currLevel < galaxyRef.current.numPlanets)
+      setCurrLevel(prevLevel => prevLevel + 1)
   }
 
   const handlePrev = e => {
@@ -38,26 +41,27 @@ const Planet = () => {
   }
 
   const selectAndDrawPlanet = () => {
-    const planet = getPlanet(galaxyRef.current, indexRef.current, currLevel)
+    const planet = getPlanet(
+      galaxyRef.current.planetsBuffer,
+      galaxyRef.current.planetsIndex,
+      currLevel
+    )
     setCanvasWidth(planet.worldwidth)
     setCanvasHeight(planet.worldheight)
     drawPlanet(planet)
     console.log(planet)
   }
 
-  const drawPlanet = ({ lines, worldwidth, worldheight }) => {
+  const drawPlanet = planet => {
     const ctx = canvasEl.current.getContext("2d")
     ctx.save()
     ctx.fillStyle = "#000"
     ctx.beginPath()
-    ctx.fillRect(0, 0, worldwidth, worldheight)
+    ctx.fillRect(0, 0, planet.worldwidth, planet.worldheight)
     ctx.stroke()
     ctx.strokeStyle = "#FFF"
     ctx.lineWidth = 2
-    for (let line of lines) {
-      //necessary because original app assumed everything past a
-      //the first 'wrong' entry was also 'wrong'
-      if (line.startx === 10000) break
+    for (let line of planet.lines) {
       ctx.beginPath()
       let color
       switch (line.kind) {
@@ -85,116 +89,15 @@ const Planet = () => {
     ctx.restore()
   }
 
-  // Header structure:
-  // first word (2 bytes) = ffef i.e. -17 in 2s complement
-  // second word = number of planets
-  // third word = the "cartoon", i.e., demo,  planet
-  // fourth word = nothing?
-  // fifth word = nothing?
-  // sixth word = start of index
-  // index is 150 chars/bytes, aka, 75 words
-  // so first planet should start at 80th word/160th byte
-  // planets are indexed, so the first planet in the file isn't
-  // necessarilty the first level.  to find the first level you go
-  // into the first char in the index and use that as an offset using
-  // the PLANSIZE variable
-
-  const readPlanetsHeader = galaxy => {
-    if (galaxy[0] !== 255 && galaxy[1] !== 239) {
-      setErrorText("Not a valid galaxy file")
-      setShowError(true)
-    } else {
-      setNumPlanets(galaxy[3])
-      setDemoPlanet(galaxy[5])
-      const index = galaxy.slice(10, 10 + galaxy[3])
-      indexRef.current = index
-      galaxyRef.current = galaxy
-      setShowError(false)
-      setShowControls(true)
-      setCurrLevel(1)
-    }
-  }
-
-  const getPlanet = (galaxy, index, level) => {
-    const planetStart =
-      GW.planetFile.FILEHEAD + GW.planetFile.PLANSIZE * index[level - 1]
-    // get the planet bit as a dataview as each attribute is stored
-    // as a 16bit integer, but they are big-endian because this was
-    // 68k architecture
-    const planetData = new DataView(
-      galaxy.slice(planetStart, planetStart + GW.planetFile.PLANSIZE).buffer
-    )
-    let oldIntSize = 2 //back then C ints on the mac were 2 bytes, big-endian
-    let ip = 0 //like the old code, we are going to walk a pointer over the array
-    let planetObj = {}
-    planetObj.worldwidth = planetData.getInt16(ip, false) //get two bytes, big-endian
-    planetObj.worldheight = planetData.getInt16((ip += oldIntSize), false)
-    planetObj.worldwrap = planetData.getInt16((ip += oldIntSize), false)
-    planetObj.shootslow = planetData.getInt16((ip += oldIntSize), false)
-    planetObj.xstart = planetData.getInt16((ip += oldIntSize), false)
-    planetObj.xstart %= planetObj.worldwidth
-    planetObj.ystart = planetData.getInt16((ip += oldIntSize), false)
-    planetObj.planetbonus = planetData.getInt16((ip += oldIntSize), false)
-    planetObj.gravx = planetData.getInt16((ip += oldIntSize), false)
-    planetObj.gravy = planetData.getInt16((ip += oldIntSize), false)
-    planetObj.numcraters = planetData.getInt16((ip += oldIntSize), false)
-
-    ip = GW.planetFile.PLANHEAD - oldIntSize
-    planetObj.lines = []
-    let i
-    for (i = 0; i < GW.game.NUMLINES; i++) {
-      let line = {
-        startx: planetData.getInt16((ip += oldIntSize), false), //left endpoint x
-        starty: planetData.getInt16((ip += oldIntSize), false), //left endpoint y
-        length: planetData.getInt16((ip += oldIntSize), false) //longest of x, y
-      }
-      let ud_and_t = planetData.getInt16((ip += oldIntSize), false)
-      line.up_down = ud_and_t >> 8 //top eight bits - 1 for down, -1 for up
-      line.type = ud_and_t & 7 //bottom 3 bits - n, nne, ne, ene, e
-      line.kind = (ud_and_t & 31) >> 3 // 2 bits above bottom 3 bits - normal, bounce, ghost, explode
-      if (line.type === GW.type.LINE_NNE || line.type === GW.type.LINE_ENE)
-        line.length |= 1 //make length an odd number
-      //rough version of pythagorean theorum using sin/cos tables above
-      line.endx = line.startx + ((xlength[line.type] * line.length) >> 1)
-      line.endy =
-        line.starty + line.up_down * ((ylength[line.type] * line.length) >> 1)
-      //convert to newtype that eliminates up/down -- before south was north, down etc
-      line.newType = line.up_down === -1 ? 10 - line.type : line.type
-      if (!line.type || line.endx > 4000 || line.starty > 4000)
-        line.startx = 10000
-      planetObj.lines.push(line)
-    }
-    planetObj.bunkers = []
-    for (i = 0; i < GW.game.NUMBUNKERS; i++) {
-      let bunker = {
-        x: planetData.getInt16((ip += oldIntSize), false),
-        y: planetData.getInt16((ip += oldIntSize), false),
-        rot: planetData.getInt16((ip += oldIntSize), false),
-        range1: planetData.getInt16((ip += oldIntSize), false),
-        range2: planetData.getInt16((ip += oldIntSize), false),
-        range3: planetData.getInt16((ip += oldIntSize), false),
-        range4: planetData.getInt16((ip += oldIntSize), false)
-      }
-      planetObj.bunkers.push(bunker)
-    }
-    planetObj.fuels = []
-    for (i = 0; i < GW.game.NUMFUELS; i++) {
-      let fuel = {
-        x: planetData.getInt16((ip += oldIntSize), false),
-        y: planetData.getInt16((ip += oldIntSize), false)
-      }
-      planetObj.fuels.push(fuel)
-    }
-
-    return planetObj
-  }
-
   let fileReader
 
   const handleFileLoaded = () => {
     const blob = fileReader.result
-    const galaxyFile = new Uint8Array(blob)
-    readPlanetsHeader(galaxyFile)
+    galaxyRef.current = parseGalaxyFile(blob)
+    setCurrLevel(1)
+    setShowControls(true)
+    setShowError(false)
+    //handle error if invalid galaxy
   }
 
   const handleFileUploaded = file => {
@@ -211,27 +114,29 @@ const Planet = () => {
           onChange={e => handleFileUploaded(e.target.files[0])}
         />
         {showError && <div>Error: {errorText}</div>}
-        <ul>
-          <li>Number of Planets: {numPlanets}</li>
-          <li>Demo/Cartoon Planet: {demoPlanet}</li>
-        </ul>
       </form>
       {showControls && (
-        <form className="select-planet">
-          <input
-            onChange={e => setCurrLevel(e.target.value)}
-            value={currLevel}
-            type="number"
-            min="1"
-            max={numPlanets}
-          />
-          <input
-            type="button"
-            value="<- Previous Planet"
-            onClick={handlePrev}
-          />
-          <input type="button" value="Next Planet ->" onClick={handleNext} />
-        </form>
+        <>
+          <ul>
+            <li>Number of Planets: {galaxyRef.current.numPlanets}</li>
+            <li>Demo/Cartoon Planet: {galaxyRef.current.demoPlanet}</li>
+          </ul>
+          <form className="select-planet">
+            <input
+              onChange={e => setCurrLevel(e.target.value)}
+              value={currLevel}
+              type="number"
+              min="1"
+              max={galaxyRef.current.numPlanets}
+            />
+            <input
+              type="button"
+              value="<- Previous Planet"
+              onClick={handlePrev}
+            />
+            <input type="button" value="Next Planet ->" onClick={handleNext} />
+          </form>
+        </>
       )}
       <canvas ref={canvasEl} width={canvasWidth} height={canvasHeight} />
     </div>
---
+++ src/planet-parser.js
@@ -0,0 +1,211 @@
+/* File: planet-parser.js
+ *
+ * This file contains javascript routines for parsing "Galaxy"
+ * files used by the Macintosh game Continuum written by Randy and
+ * Brian Wilson.
+ *
+ * The code is based off the source code released into the public
+ * domain by the Wilson brothers at http://www.ski-epic.com/continuum_downloads/
+ * Specifically, this file is based on the definitions and routines
+ * in GW.h, Main.c, and Edit.c
+ *
+ * I've tried to stick to the original code as much as possible so that this can
+ * be used with ports of other parts of the Continuum code and so you get a
+ * flavor of what it was like to program on these older machines.
+ *
+ * Sam Davidoff - 2020
+ */
+
+//Continuum was written for the original Macintosh which ran
+//on a Motorola 68000 processor which was big-endian.
+//This function pulls a big-endian 2-byte integer from
+//offset i in DataView dv.
+const getInt16BE = (dv, i) => dv.getInt16(i, false)
+
+//The Continuum galaxy file contained five parts:
+//(1) a file identifier
+//(2) a count of the number of planets
+//(3) the demo or "cartoon" planet
+//(4) an index to the address of each planet
+//(5) the planets
+
+const parseGalaxyFile = galaxyBuffer => {
+  const galaxyFileIdentifier = -17
+  const planetIndexByteOffset = 10
+  const galaxyHeaderBytes = 160
+  const galaxyDV = new DataView(galaxyBuffer)
+
+  if (getInt16BE(galaxyDV, 0) !== galaxyFileIdentifier) {
+    //throw an error
+  } else {
+    const numPlanets = getInt16BE(galaxyDV, 2)
+    const demoPlanet = getInt16BE(galaxyDV, 4)
+    const indexByteEnd = planetIndexByteOffset + numPlanets //1 byte per planet entry
+    const planetsIndex = new Uint8Array(
+      //1 byte per entry so endianness doesn't matter
+      galaxyBuffer.slice(planetIndexByteOffset, indexByteEnd)
+    )
+    const planetsBuffer = galaxyBuffer.slice(galaxyHeaderBytes)
+    return { numPlanets, demoPlanet, planetsIndex, planetsBuffer }
+  }
+}
+
+const getPlanet = (planetsBuffer, planetIndex, planet) => {
+  const planetByteSize = 1540
+  const planetHeaderSize = 30
+  const planetByteOffset = planetByteSize * planetIndex[planet - 1]
+  const planetByteEnd = planetByteOffset + planetByteSize
+  const planetDV = new DataView(
+    planetsBuffer.slice(planetByteOffset, planetByteEnd)
+  )
+  //This does basically the same thing the Continuum code did to "unpack"
+  //a planet--start with a pointer 'ip' and walk it down the stream of bits
+  //one long int (which was 2 bytes) at a time, occassionaly using bitwise
+  //operators when information was stored in single bytes or flags.
+  //Here 'ip' is our pointer and 'incr' reflects that by default we will pull
+  //2 bytes at a time
+  //NB: this would have been easier to do with a Int16 array but we can't because
+  //the file is big-endian and most machines today are not...
+  let ip = 0
+  let incr = 2
+  let planetObj = {}
+
+  planetObj.worldwidth = getInt16BE(planetDV, ip)
+  planetObj.worldheight = getInt16BE(planetDV, (ip += incr))
+  planetObj.worldwrap = getInt16BE(planetDV, (ip += incr))
+  planetObj.shootslow = getInt16BE(planetDV, (ip += incr))
+  planetObj.xstart = getInt16BE(planetDV, (ip += incr))
+  planetObj.xstart %= planetObj.worldwidth //I don't know why; it's what the old code did...
+  planetObj.ystart = getInt16BE(planetDV, (ip += incr))
+  planetObj.planetbonus = getInt16BE(planetDV, (ip += incr))
+  planetObj.gravx = getInt16BE(planetDV, (ip += incr))
+  planetObj.gravy = getInt16BE(planetDV, (ip += incr))
+  planetObj.numcraters = getInt16BE(planetDV, (ip += incr))
+
+  ip = planetHeaderSize - incr //start 2 bytes lower to avoid a kludge in the for loop start
+
+  //A Continuum planet is composed of walls (called 'lines'), bunkers, fuel cells, and
+  //craters.  The constants below are important because the filesize was hardcoded to allow
+  //these many per planet.
+  const maxLines = 125
+  const maxBunkers = 25
+  const maxFuels = 15
+  const maxInitCraters = 25 //bunkers turn into craters when shot so eventually there could be 50
+
+  //All lines in Continuum are defined as left to right lines in the northeast
+  //quadrant of a cartesian graph.  They can go out at one of five angles, designated
+  //with the 'type' of N, NNE, NE, ENE, and E.  The actual line, however, can be drawn
+  //'up' (i.e., in the northeast quadrant) or 'down" (i.e., in the southeast quadrant).
+  //In the latter case, the lines are really going (S, SSE, SE, etc.).  Whether a line is
+  //'up' or 'down' is represented by 'up_down', which is -1 for up and 1 for down.
+  planetObj.lines = []
+  let garbage = false
+  for (let i = 0; i < maxLines; i++) {
+    let line = {
+      startx: getInt16BE(planetDV, (ip += incr)),
+      starty: getInt16BE(planetDV, (ip += incr)),
+      length: getInt16BE(planetDV, (ip += incr))
+    }
+    //The next two bytes are packed as follows:  the top byte indicates if the line is
+    //'up' (-1) or 'down' (1).  The bottom 3 bits of the lower byte indicate which of the
+    //five directions in a quadrant (or angles, if you like) the line is pointing.  The
+    //two bits above that indicate which 'kind' of line it is: (1) normal, (2) bounce,
+    //(3) phantom, or (4) explode.  NB: I don't think the latter type was actually supported.
+    let ud_and_t = getInt16BE(planetDV, (ip += incr))
+    line.up_down = ud_and_t >> 8
+    line.type = ud_and_t & 7
+    line.kind = (ud_and_t & 31) >> 3
+    //Continuum pre-calculated some things about lines, presumably for speed, including
+    //using a very rough version of sine and cosine tables for the 5 angles at issue
+    //(multiplied by two to give a little extra precision, i.e., an extra bit).
+    const ylength = [0, 2, 2, 2, 1, 0]
+    const xlength = [0, 0, 1, 2, 2, 2]
+    line.endx = line.startx + ((xlength[line.type] * line.length) >> 1)
+    line.endy =
+      line.starty + line.up_down * ((ylength[line.type] * line.length) >> 1)
+    //This appears to be making lines from these two angles always odd, but
+    //I'm not sure why...
+    const LINE_NNE = 2
+    const LINE_ENE = 4
+    if (line.type === LINE_NNE || line.type === LINE_ENE) line.length |= 1
+    //newType consolidates up_down and type to give the following directions:
+    //S, SSE, SE, ESE, E, ENE, NE, NNE
+    line.newType = line.up_down === -1 ? 10 - line.type : line.type
+    //Setting line.startx to 10000  was how Continuum indicated that a line
+    //and _all subsequent lines in the buffer_ were invalid (it didn't
+    //actually zero out the later lines).
+    if (!line.type || line.endx > 4000 || line.starty > 4000)
+      line.startx = 10000
+    //Since every line past the first line where startx is 10000 is garbage
+    //we'll ignore them.  You can get rid of this to see the random lines that
+    //were there in the original Continuum files.
+    garbage = line.startx === 10000 ? true : garbage
+    if (!garbage) planetObj.lines.push(line)
+  }
+
+  planetObj.bunkers = []
+  garbage = false
+  let bunker
+  for (let i = 0; i < maxBunkers; i++) {
+    bunker = {
+      x: getInt16BE(planetDV, (ip += incr)),
+      y: getInt16BE(planetDV, (ip += incr)),
+      rot: getInt16BE(planetDV, (ip += incr)),
+      ranges: [],
+      alive: true
+    }
+    for (let j = 0; j < 4; j++) {
+      bunker.ranges.push(getInt16BE(planetDV, (ip += incr)))
+    }
+    //A rotation ('rot') value of -1 means the bunker is 'kind' 0,
+    //which doesn't have a rotation [or gets it from the wall?]  Otherwise,
+    //the 'kind' of bunker is stored in the top byte and the rotation in the
+    //bottom byte of the 'rot' 2 byte integer
+    if (bunker.rot === -1) bunker.kind = 0
+    else {
+      bunker.kind = bunker.rot >> 8
+      bunker.rot &= 255
+    }
+    //This is what's in the original code, but it can't be right or every
+    //kind 0 would be ignored...
+    //if (bunker.rot < 0 || bunker.x > 4000 || bunker.y > 4000) bunker.x = 10000
+    if (bunker.x > 4000 || bunker.y > 4000) bunker.x = 10000
+
+    garbage = bunker.x === 10000 ? true : garbage
+    if (!garbage) planetObj.bunkers.push(bunker)
+  }
+
+  planetObj.fuels = []
+  garbage = false
+
+  let fuel
+  for (let i = 0; i < maxFuels; i++) {
+    fuel = {
+      x: getInt16BE(planetDV, (ip += incr)),
+      y: getInt16BE(planetDV, (ip += incr)),
+      alive: true,
+      currentfig: 1, //these two are for animation
+      figcount: 1
+    }
+    if (fuel.x > 4000 || fuel.y > 4000) fuel.x = 10000
+
+    garbage = fuel.x === 10000 ? true : garbage
+    if (!garbage) planetObj.fuels.push(fuel)
+  }
+  //  planetObj.fuels[maxFuels - 1].x = 20000 //i don't know why this is here; was in the original code
+
+  planetObj.craters = []
+
+  let crater
+  for (let i = 0; i < maxInitCraters; i++) {
+    crater = {
+      x: getInt16BE(planetDV, (ip += incr)),
+      y: getInt16BE(planetDV, (ip += incr))
+    }
+    planetObj.craters.push(crater)
+  }
+
+  return planetObj
+}
+
+export { parseGalaxyFile, getPlanet }
